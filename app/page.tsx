e15f97364b62da7bfacd7b144f16951dc64d2b97
YuichiOta <rb19780519@gmail.com>
2024-07-02 10:09:07 +0900

    page.tsx className修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MailForm from "../components/MailForm/MailForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <h2 className="font-semibold text-3xl mb-4">お問い合わせフォーム</h2>
       <MailForm />
     </main>
